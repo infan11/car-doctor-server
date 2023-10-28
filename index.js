a402599d71851666d9b08fe2e1b3b9f38ba4a01b
infan11 <infanjiounrahman20606@gmail.com>
2023-10-28 20:21:49 +0600

    added all file

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
-// car-doctor
-//ZrONIVAyRwF53aPN
 
 
+// const servicesCollection =  client.db('carDoctor').collection('services')
+const servicesCollection = client.db('carDoctor').collection('services')
+// api
+
+app.get("/services", async(req,res ) => {
+    const cursor = servicesCollection.find()
+    const result = await cursor.toArray();
+    res.send(result)
+})
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lopynog.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,7 +41,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
